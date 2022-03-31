d54b9da0f886c864e2510cbc951a46f1923c1206
andy zhen <andyzhen44@gmail.com>
2022-03-30 20:31:03 -0700

    implement ajax to post and get requests async

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,52 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement =  (tweetData) => {
   const $article = $(`<article>`).addClass("tweet-container");
 
   //Creates the Header of the Tweet------------------------
-  const tweetHeader = $(`<header>`).addClass("tweetHeader")
+  const tweetHeader = $(`<header>`).addClass("tweetHeader");
   const nameAndPic = $(`<div id="nameAndPic">`);
   const avatar = $(`<img class="avatar" src=${tweetData.user.avatars} />`);
   const fullName = $('<span>').addClass('fullName').text(`${tweetData.user.name}`);
-  nameAndPic.append(avatar,fullName)
+  nameAndPic.append(avatar,fullName);
   const handle = $(`<span class="tagName"></span>`).text(`${tweetData.user.handle}`);
   //--------------------------------------------------------
 
@@ -57,35 +20,45 @@ const createTweetElement =  (tweetData) => {
 
   //Footer of Tweer
   const tweetFooter = $(`<footer>`).addClass('tweetFooter');
-  const date = $(`<span>`).text(`${tweetData.created_at}`)
+  const date = $(`<span>`).text(`${tweetData.created_at}`);
   const icons = $(`<div>`).addClass('icons');
-  icons.append(`<i class="fa-solid fa-flag"></i>`, `<i class="fa-solid fa-retweet"></i>`,`<i class="fa-solid fa-heart"></i>`)
-  tweetFooter.append(date,icons)
+  icons.append(`<i class="fa-solid fa-flag"></i>`, `<i class="fa-solid fa-retweet"></i>`,`<i class="fa-solid fa-heart"></i>`);
+  tweetFooter.append(date,icons);
 
-
-
-  
-  
-
-  let res = $article.append(tweetHeader.append(nameAndPic,handle),content,tweetFooter)
-  //console.log(res)
-  return res
-    
-}
+  let res = $article.append(tweetHeader.append(nameAndPic,handle),content,tweetFooter);
+  return res;
+};
 
 const renderTweet = (data) => {
   for (const x of data) {
-    console.log(createTweetElement(x))
-    $('.tweets-container').append(createTweetElement(x))
-  }
-}
+    $('.tweets-container').prepend(createTweetElement(x))
+  };
+};
+
+const loadTweets = () => {
+  $.ajax({
+    url: '/tweets',
+    type: 'GET',
+  }).then((data) => {
+    renderTweet(data);
+
+  })
+} 
 
 $(() => {
-const $tweet = createTweetElement(tweetData);
-renderTweet(data)
-// Test / driver code (temporary)
-
-//$('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  loadTweets();
 
 
+  $("form").on('submit', function (event) {
+    console.log(event)
+    event.preventDefault();
+    $.ajax({
+      url: '/tweets/',
+      type: 'POST',
+      data: $('form').serialize()
+   
+    }).then((data) => {
+      loadTweets();
+    })
+  })
 });
